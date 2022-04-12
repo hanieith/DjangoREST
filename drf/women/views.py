c8e071c544a196ab2bc9100dd1c2b1939796e8ee
Constantin Shkutkov <shkutkoff@gmail.com>
2022-04-12 22:10:35 +0300

    12.04

--- drf/women/views.py
+++ drf/women/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Women
 from .serializers import WomenSerializer
+from django.forms import model_to_dict
 
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+class WomenAPIView(APIView):
+    def get(self, request):
+        lst = Women.objects.all().values()
+        return Response({'posts':WomenSerializer(lst, many=True).data})
+
+    def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        post_new = Women.objects.create(
+            title = request.data['title'],
+            content = request.data['content'],
+            cat_id = request.data['cat_id'],
+        )
+        return Response({'post':WomenSerializer(post_new).data})
+
+#class WomenAPIView(generics.ListAPIView):
+#queryset = Women.objects.all()
+#serializer_class = WomenSerializer
