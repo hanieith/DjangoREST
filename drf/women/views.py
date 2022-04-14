f7fea48cb51656b274c1bae106b7c9ecd62ab7db
Constantin Shkutkov <shkutkoff@gmail.com>
2022-04-14 20:49:50 +0300

    14.04

--- drf/women/views.py
+++ drf/women/views.py
@@ -2,14 +2,27 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Women
+from .models import Women, Category
 from .serializers import WomenSerializer
 from django.forms import model_to_dict
+from rest_framework.decorators import action
 
 
 class WomenViewSet(viewsets.ModelViewSet):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+        if not pk:
+            return Women.objects.all()[:3]
+        else:
+            return Women.objects.filter(pk=pk) #dont use get, becose filter return list
+
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats':cats.name})
 
 #class WomenAPIList(generics.ListCreateAPIView):
 #    queryset = Women.objects.all()
