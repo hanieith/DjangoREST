a3b9f2d489a95730bff77174c4218489f6c7b7c8
Constantin Shkutkov <shkutkoff@gmail.com>
2022-04-20 21:26:07 +0300

    20.04

--- drf/drf/urls.py
+++ drf/drf/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from women.views import *
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 #router = routers.DefaultRouter()
 #router.register(r'women', WomenViewSet) #write basename= if in views dont have queryset
@@ -16,6 +17,9 @@ urlpatterns = [
     path('api/v1/womendelete/<int:pk>', WomenAPIDestroy.as_view()),
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 #urlpatterns = [
